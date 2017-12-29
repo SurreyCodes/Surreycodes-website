ccc68f73d71e8ab2ce7eb6d1eef01261a9424f54
crathor <codyrathor@gmail.com>
2017-12-28 22:11:45 -0800

    add React App file

---
+++ surreycodes/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+ReactDOM.render(<Router>
+  <div>
+    <Route exact path="/" component={App}/>
+  </div>
+
+
+</Router>
+
+, document.getElementById('root'));
+registerServiceWorker();
