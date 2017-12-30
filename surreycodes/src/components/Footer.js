80e88ab6fa2a39df3aeedbbe57a326eafc10fddc
crathor <codyrathor@gmail.com>
2017-12-30 14:18:35 -0800

    create all components, mv to components directory and link, create cards, update headerlinks, update about us.

---
+++ surreycodes/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render(){
+    return(
+      <div>
+        <footer id="footer" className="container-fluid">
+          <div className="container">
+            <ul>
+              <li>Our Code of Conduct</li>
+              <li>Terms of use for members</li>
+              <li>Sponsor Us</li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ surreycodes/src/components/Hook.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Hook extends Component {
+  render(){
+    return(
+      <div id="hook" className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="text-center text-primary">PEER TO PEER MENTORING<br />TECH COMMUNITY</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hook;
