7017f72b72dc11f79c69e8b4268a50fda3732cf3
crathor <codyrathor@gmail.com>
2018-01-03 22:04:16 -0800

    update header - animation to follow

--- surreycodes/src/components/Header.js
+++ surreycodes/src/components/Header.js
@@ -6,31 +6,29 @@ class Header extends Component {
   render(){
     return(
       <header id="header" >
-        <nav id="navbar" >
-          <Link id="logo" to="/">
-            <img src={Logo} width="30" height="30" alt="SurreyCodes"/>
-          </Link>
-            <ul id="navbar-nav">
+        <Link id="logo" to="/">
+          <img src={Logo} width="30" height="30" alt="SurreyCodes"/>
+        </Link>
+            <ul id="navbar">
               <li>
-                <Link to="/">Home</Link>
+                <Link className="nav-link" to="/">Home</Link>
               </li>
               <li>
-                <Link to="/meetup">Meetup</Link>
+                <Link className="nav-link" to="/meetup">Meetup</Link>
               </li>
               <li>
-                <Link to="/about">About Us</Link>
+                <Link className="nav-link" to="/about">About Us</Link>
               </li>
               <li>
-                <Link to="/events">Events</Link>
+                <Link className="nav-link" to="/events">Events</Link>
               </li>
               <li>
-                <Link to="/about">Members</Link>
+                <Link className="nav-link" to="/about">Members</Link>
               </li>
               <li>
-                <Link to="/sponsors">Sponsors</Link>
+                <Link className="nav-link" to="/sponsors">Sponsors</Link>
               </li>
             </ul>
-        </nav>
       </header>
     );
   }
