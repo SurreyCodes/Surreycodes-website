9e2c72d1b14c082992dd4ddc59ff70baa9cdd8d8
Luis Espinal <lespinalrivera@gmail.com>
2018-01-13 11:57:42 -0800

    adding card wrap and fulltime program landing page

---
+++ surreycodes/src/components/Fulltimeprogram.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+class FulltimeProgram extends Component {
+  render(){
+    return(
+      <div>
+        <div id="FulltimeProgram">
+          <div className="container">
+          <section className="landingSection">
+            <div>
+              <h1>Study with Us</h1>
+              <button>Enroll Today</button>
+            </div>
+            <div>separation line</div>
+            <div>
+              <h1>Non Profit Graduation Project</h1>
+              <button>Submit One</button>
+            </div>
+          </section>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
