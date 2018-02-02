c443a34a81a1de0a4468ed0b522647f6fab2a2c9
Luis Espinal <lespinalrivera@gmail.com>
2018-02-01 22:06:53 -0800

    Adding more sections to the fulltime program landing page

--- surreycodes/src/components/Fulltimeprogram-Landing.js
+++ surreycodes/src/components/Fulltimeprogram-Landing.js
@@ -5,49 +5,59 @@ import SurreyCodesMembers from '../assets/SurreycodesMembers.svg';
 import IconCup from '../assets/Icon - Blue Cup.svg';
 import IconHeart from '../assets/Icon - Green Heart.svg';
 import IconPartners from '../assets/Icon - Green partner.svg';
+import IconSpeaker from '../assets/Icon Speaker.svg';
 
 class FulltimeProgram extends Component {
   render(){
-    return(
-      <div>
-      <Header />
-        <div id="FulltimeProgram">
-          <div className="container">
-          <section className="landingSection">
-              <img src={SurreyCodesMembers} alt="Our Campers Studying and Nonprofit can submit their projects" className="fulltime-program-hero"/>
-            <div>
-              <h1>LEARN TO CODE FOR FREE</h1>
-              <h2>By Supporting Non-Profit Projects</h2>
-            </div>
-            <div>
-                <button>KickStart Campaign</button>
-                <button>See Available Projects</button>
-            </div>
-          </section>
+      return <div>
+          <Header/>
+          <div id="FulltimeProgram">
               <section className="landingSection">
-              <h1>Community Benefits</h1>
-              <div><img src={IconCup} alt="Cup of Victory!"/>
-                  <h3>Students</h3>
-                  <p>Get from new coder to Full Stack and Job ready for free with a community to back your knowledge.</p>
-              </div>
-              <div><img src={IconHeart} alt="Heart of Gold!"/>
-                  <h3>Non Profits</h3>
-                  <p>Focus your resources to help our communities most in need and less in webdevelopment.</p>
-              </div>
-              <div><img src={IconPartners} alt="Partner and help!"/>
-                  <h3>Partner Companies</h3>
-                  <p>Hire Full Stack developers with a record of teawork and self-learning that have done coding for good.</p>
-              </div>
+                  <img src={SurreyCodesMembers} alt="Our Campers Studying and Nonprofit can submit their projects" />
+                  <div className="title-landingSection">
+                      <h1>LEARN TO CODE FOR FREE</h1>
+                      <h3>By Supporting Non-Profit Projects</h3>
+                      <button>KickStart Campaign</button>
+                      <button>See Available Projects</button>
+                  </div>
               </section>
-          <section>
-            <h1>Learn To Code for free</h1>
-            <p>Learn to code with others in a great space, while you help non profits with their technological approach.</p>
-          </section>
-            </div>
-        </div>
-      <Footer />
-      </div>
-    );
+              <div className="container">
+                  <section className="feature">
+                      <h1>Community Benefits</h1>
+                      <img src={IconCup} alt="Cup of Victory!"/>
+                          <h3>Students</h3>
+                          <p>Get from new coder to Full Stack and Job ready for free with a community to back your
+                              knowledge.</p>
+                          <img src={IconHeart} alt="Heart of Gold!"/>
+                          <h3>Non Profits</h3>
+                          <p>Focus your resources to help our communities most in need and less in webdevelopment.</p>
+                          <img src={IconPartners} alt="Partner and help!"/>
+                          <h3>Partner Companies</h3>
+                          <p>Hire Full Stack developers with a record of teawork and self-learning that have done coding
+                              for
+                              good.</p>
+                  </section>
+                  <section className="calltoaction">
+                      <img src={IconSpeaker} alt="A Big interesting speaker!"/>
+                      <p>British Columbia’s Non-profits spends about $100 MM dollars every year in tech support. Those are $100MM dollars that could go towards the most in need.</p>
+                  </section>
+                  <section className="subscribe">
+                      <h1>Help us raise the funds we need to make this come true</h1>
+                      <input type="email" placeholder="example@surreycodes.com"></input>
+                      <button className="cr-scan-ct" type="submit">SUBSCRIBE</button>
+                  </section>
+                  <section className="backers">
+                      <h1>These companies are already involved:</h1>
+                      <a href="#"><img src="#" alt="Beta Collective"/></a>
+                      <a href="#"><img src="#" alt="Microsoft Corporation"/></a>
+                      <a href="#"><img src="#" alt=""/></a>
+                      <a href="#"><img src="#" alt=""/></a>
+                      <a href="#"><img src="#" alt=""/></a>
+                  </section>
+              </div>
+          </div>
+          <Footer/>
+      </div>;
   }
 }
 
