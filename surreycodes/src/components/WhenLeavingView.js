b6dde185188cae3a1f21d740ede9acb9e0221fd5
Cody Rathor <crathor@Codys-MacBook-Pro.local>
2018-07-07 08:55:03 -0700

    code clean up in App.js

--- surreycodes/src/components/WhenLeavingView.js
+++ surreycodes/src/components/WhenLeavingView.js
@@ -1,40 +1,34 @@
-import React, { Component } from 'react';
-import Waypoint from 'react-waypoint';
+import React, { Component } from 'react'
+import Waypoint from 'react-waypoint'
 
 class WhenLeavingView extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLeavingView: false
-    };
-    this.onLeave = this.onLeave.bind(this);
-    this.onEnter = this.onEnter.bind(this);
+  state = {
+    isLeavingView: false
   }
 
-  onLeave({ currentPosition }){
+  onLeave = ({ currentPosition }) => {
     if (currentPosition === Waypoint.above) {
       this.setState({
         isLeavingView: true
-      });
+      })
     }
   }
-  onEnter({ previousPosition }){
+  onEnter = ({ previousPosition }) => {
     if (previousPosition === Waypoint.above) {
       this.setState({
         isLeavingView: false
-      });
+      })
     }
   }
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <Waypoint onLeave={this.onLeave}></Waypoint>
-        <Waypoint onEnter={this.onEnter}></Waypoint>
-        {this.props.children({isLeavingView: this.state.isLeavingView})}
+        <Waypoint onLeave={this.onLeave} />
+        <Waypoint onEnter={this.onEnter} />
+        {this.props.children({ isLeavingView: this.state.isLeavingView })}
       </div>
-    );
+    )
   }
 }
 
-export default WhenLeavingView;
+export default WhenLeavingView
--- surreycodes/src/index.js
+++ surreycodes/src/index.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Sponsors from './containers/Sponsors/Sponsors';
-import FulltimeProgram from './containers/FulltimeProgram/FulltimeProgram';
-import CodeofConduct from "./containers/CodeofConduct/CodeofConduct";
-import Header from './components/Header/Header';
-import WhenLeavingView from './components/WhenLeavingView';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import Sponsors from './containers/Sponsors/Sponsors'
+import FulltimeProgram from './containers/FulltimeProgram/FulltimeProgram'
+import CodeofConduct from './containers/CodeofConduct/CodeofConduct'
+import Header from './components/Header/Header'
+import WhenLeavingView from './components/WhenLeavingView'
 
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
 
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 ReactDOM.render(
   <Router>
     <div>
-    <WhenLeavingView>
-          {({ isLeavingView }) =>
-            <Header animate={isLeavingView}/>
-          }
-        </WhenLeavingView>
-      <Route exact path="/" component={App}/>
-      <Route path="/sponsors" component={Sponsors}/>
-      <Route path="/fulltime" component={FulltimeProgram}/>
-      <Route path="/codeofconduct" component={CodeofConduct}/>
-      <Route path="/community" component={CodeofConduct}/>
+      <WhenLeavingView>
+        {({ isLeavingView }) => <Header animate={isLeavingView} />}
+      </WhenLeavingView>
+      <Route exact path="/" component={App} />
+      <Route path="/sponsors" component={Sponsors} />
+      <Route path="/fulltime" component={FulltimeProgram} />
+      <Route path="/codeofconduct" component={CodeofConduct} />
+      <Route path="/community" component={CodeofConduct} />
     </div>
-  </Router>
-, document.getElementById('root'));
-registerServiceWorker();
+  </Router>,
+  document.getElementById('root')
+)
+registerServiceWorker()
