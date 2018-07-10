65e8466c5500f626253455dd95d49df593fb2c35
Luis Espinal <lespinalrivera@gmail.com>
2018-07-10 12:36:37 -0700

    Add Sponsors content, sponsors style

--- surreycodes/src/containers/Sponsors/Sponsors.js
+++ surreycodes/src/containers/Sponsors/Sponsors.js
@@ -9,21 +9,12 @@ class Sponsors extends Component {
   render() {
     return (
       <div id="Sponsors">
-        <section className="calltoaction">
-          <img src={IconSpeaker} alt="A Big interesting speaker!" />
-          <h3>
-            British Columbia’s Non-profits spends about $100 MM dollars every
-            year in tech support. Those are $100MM dollars that could go towards
-            the most in need.
-          </h3>
-          <h1>We are changing this. Want to help?</h1>
-        </section>
-        <section className="howtohelp">
+          <h1>We generate a 10x effect with every contribution received.</h1>
+          <h2>High quality developers are graduating from our program. <br/><br/>Non-profits will have more budget to do good.</h2>
+          <section className="howtohelp">
           <h1>We are preparing the developers of the future</h1>
           <p>Our students learn by doing. They prepare, code and improve web applications for good. By doing this, non-profits in British Columbia benefits of having low-cost websites and apps. At the same time, our students build a portfolio of real-wold experiences.</p>
           <p>Each place in our full time program costs about $4,000.00 to provide. We do our best to keep these costs low and cultivate high quality developers at the same time.</p>
-          <h1>Your contribution creates a 10x effect.</h1>
-          <p>High quality graduates will look up to your company. And non-profits will have more budget to do good.</p>
           <h1>Every year, we accept a limited amount of Sponsors</h1>
         </section>
         <section className="backers">
@@ -43,11 +34,19 @@ class Sponsors extends Component {
           <a href="https://typow.ca">
             <img src={Typow} width="80" height="80" alt="Typow" />
           </a>
-            <h3>Know a Non-Profit that could benefit from our program? tell them to submit a project</h3>
-            <NavLink to='/'>
-                <button className="main-button">Non-Profits</button>
-            </NavLink>
         </section>
+            <section className="calltoaction">
+                <img src={IconSpeaker} alt="A Big interesting speaker!" />
+                <h2>
+                    Non-Profits spend $100 MM dollars per year in technology support.<br/> Those are $100MM dollars that could go towards the most in need.
+                </h2>
+        </section>
+          <section className="nonprofit-referral">
+              <h3>Know a Non-Profit that could benefit from our program? tell them to submit a project</h3>
+              <NavLink to='/'>
+                  <button className="main-button">Non-Profits</button>
+              </NavLink>
+          </section>
       </div>
     )
   }
