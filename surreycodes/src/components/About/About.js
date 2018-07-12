bd06fa8e68f187838f7d611bc95a25dbe1bb53c9
Luis Espinal <lespinalrivera@gmail.com>
2018-07-11 21:40:34 -0700

    add more content to About section, created ContactForm component for future tasks

--- surreycodes/src/components/About/About.js
+++ surreycodes/src/components/About/About.js
@@ -5,12 +5,14 @@ class About extends Component {
   render(){
     return(
       <div id="about">
-        <div className="container">
-          <div className="about-flex-container">
-            <h1>About Us</h1>
+          <section className="about-intro">
+            <h1>Where it all started..</h1>
             <p>The SurreyCodes Community was created with the inspiration of <a href="https://www.freecodecamp.org/">freecodecamp.com</a> to cultivate peer-to-peer technology learning. We strive to provide a venue where developers and coders alike can gather and exchange knowledge to elevate our Non-Profit's approach to technology within British Columbia.</p>
-          </div>
-        </div>
+          </section>
+          <section className="about-why">
+            <h2>We connect new Developers with non profits</h2>
+            <p>Both of which gets a benefit. Making sure that the most benefit one is the community of most in need.</p>
+          </section>
       </div>
     );
   }
